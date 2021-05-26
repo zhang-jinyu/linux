0f4cfff2aa758ebb121d7224182b238c6c11e0b4
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-05-26 15:11:52 +0300

    adrf6780: initial commit
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ drivers/iio/frequency/adrf6780.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * ADRF6780 driver
+ *
+ * Copyright 2021 Analog Devices Inc.
+ */
+
+MODULE_AUTHOR("Antoniu Miclaus <antoniu.miclaus@analog.com");
+MODULE_DESCRIPTION("Analog Devices ADRF6780");
+MODULE_LICENSE("GPL v2");
+
